a518605a42e6fcfb5860ac474db84cd9e66ce6cc
Marconi Braga <marconi.braga@siedos.com.br>
2018-04-03 12:52:44 -0300

    Nova classe para criar arquivo

--- Topico01(19-03-2018)/manipulacao_de_arquivos/src/exibe_conteudo_arquivo_texto_alternativa/DisplayFileContentsAlternative.java
+++ Topico01(19-03-2018)/manipulacao_de_arquivos/src/exibe_conteudo_arquivo_texto_alternativa/DisplayFileContentsAlternative.java
@@ -1,17 +1,22 @@
-package exibe_conteudo_arquivo_texto;
+package exibe_conteudo_arquivo_texto_alternativa;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class DisplayFileContents {
+public class DisplayFileContentsAlternative {
 
     public static void main(String[] args) throws IOException {
-        Charset uft8 = Charset.forName("UTF-8");
+
         Path arquivo = Paths.get("/user/name_user/i-2018/Topico01(19-03-2018)/manipulacao_de_arquivos", "file.txt");
-        for (String linha : Files.readAllLines(arquivo, uft8)) {
+        Charset utf8 = Charset.forName("UTF-8");
+        BufferedReader br = Files.newBufferedReader(arquivo, utf8);
+
+        String linha;
+        while ((linha = br.readLine()) != null) {
             System.out.println(linha);
         }
 
